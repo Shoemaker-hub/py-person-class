5047f086ff8f32edc12b7a2e30ae56c7aef5c1bb
Bohdan Sventukh <b.sventukh@sxope.com>
2025-11-04 21:08:14 +0200

    first commit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    instances = []
+    for i in people:
+        new_person = Person(i["name"], i["age"])
+        instances.append(new_person)
+
+    for item in people:
+        person = Person.people[item["name"]]
+
+        if item.get("wife"):
+            wife_name = item["wife"]
+            person.wife = Person.people[wife_name]
+        if item.get("husband"):
+            husband_name = item["husband"]
+            person.husband = Person.people[husband_name]
+
+    return instances
